0bcbbbd9a19109c5d4cc24b6391ab77d00920dcb
priyanshukr449 <priyanshukr449@gmail.com>
2023-09-05 13:41:33 +0530

    recursionpid

---
+++ Recursion/printIncreasingDecreasing.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class printIncreasingDecreasing{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pdi(n);
+    }
+
+    public static void pdi(int n){
+        if(n== 0){
+            return;
+        }
+        System.out.println(n);
+        pdi(n-1);
+        System.out.println(n);
+    }
+}
